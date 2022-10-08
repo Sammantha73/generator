35f47db27a8c3394c13e109f38426f2514632937
Sammantha73 <Samanthafeijoom@gmail.com>
2022-10-08 23:22:02 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function GenerarDom() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var ext = [".com", ".net", ".es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  var position = pronoun.length;
+  var position2 = adj.length;
+  var position3 = noun.length;
+  var position4 = ext.length;
+  var midom = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (var vpro = 0; vpro < position; vpro++) {
+    for (var vadj = 0; vadj < position2; vadj++) {
+      for (var vnoun = 0; vnoun < position3; vnoun++) {
+        for (var vext = 0; vext < position4; vext++) {
+          midom.push(
+            "<br>" + pronoun[vpro].concat(adj[vadj], noun[vnoun], ext[vext])
+          );
+        }
+      }
+    }
+  }
+  document.getElementById("caja").innerHTML = midom;
+}
